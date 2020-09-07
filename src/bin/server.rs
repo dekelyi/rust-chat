cfbe5d550925c6051c5dcd291c378ea7c0827e53
Dekel Yigal <dekelyi@gmail.com>
2020-09-07 15:44:43 +0300

    refactor(server): changed to TCP, server-client module

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,8 @@
 use anyhow::{self, Context};
-use std::net::UdpSocket;
+use std::net::TcpListener;
 
 fn main() -> anyhow::Result<()> {
-    let socket = UdpSocket::bind("127.0.0.1:0").context("failed to bind adress")?;
+    let socket = TcpListener::bind("127.0.0.1:0").context("failed to bind adress")?;
     println!("listening on {}", socket.local_addr()?);
     Ok(())
 }
