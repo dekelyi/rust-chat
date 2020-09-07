776b8239f55acf0926787c2f7c72a5dca1bfe751
Dekel Yigal <dekelyi@gmail.com>
2020-09-07 15:28:07 +0300

    feat: add basic UDP socket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    todo!()
+use anyhow::{self, Context};
+use std::net::UdpSocket;
+
+fn main() -> anyhow::Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:0").context("failed to bind adress")?;
+    println!("listening on {}", socket.local_addr()?);
+    Ok(())
 }
