d698424f51fb5c0acc9159a49c6481fe761ba34f
Dekel Yigal <dekelyi@gmail.com>
2020-09-07 17:00:19 +0300

    feat(server): add logger using pretty-env-logger

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,11 +1,22 @@
 use anyhow::{self, Context};
 use tokio::net::TcpListener;
 
+/// init the logger
+/// Log level: `info` by default, use `LOG` env to change
+fn init_log() -> Result<(), log::SetLoggerError> {
+    let mut builder = pretty_env_logger::formatted_builder();
+    #[allow(clippy::or_fun_call)]
+    builder.parse_filters(&std::env::var("LOG").unwrap_or("info".to_string()));
+    builder.try_init()
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    init_log()?;
+
     let socket = TcpListener::bind("127.0.0.1:0")
         .await
         .context("failed to bind adress")?;
-    println!("listening on {}", socket.local_addr()?);
+    log::info!("listening on {}", socket.local_addr()?);
     Ok(())
 }
