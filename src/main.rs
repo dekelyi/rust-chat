b6481669c0a2ac0f81399307c17f2a4a6519d9ff
Dekel Yigal <dekelyi@gmail.com>
2020-09-09 17:05:36 +0300

    refactor(ChatServer): :art: Split `ChatServer` to `server.rs` from `main.rs`
    API

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,4 @@
-use anyhow::{self, Context};
-use tokio::{net, prelude::*, stream::StreamExt, task};
-
-struct ChatServer {
-    listener: net::TcpListener,
-}
-
-impl ChatServer {
-    async fn handle_connections(stream: &mut net::TcpStream) {
-        let mut buf = bytes::BytesMut::with_capacity(64);
-        let addr = stream.peer_addr().unwrap();
-
-        log::info!("{}: Got connection", addr);
-        loop {
-            let res = stream.read_buf(&mut buf).await;
-            let res = res.or(stream.write_buf(&mut buf).await);
-            if let Ok(0) | Err(_) = res {
-                break;
-            }
-        }
-        log::info!("{}: Connection closed", addr);
-    }
-
-    async fn run(&mut self) {
-        while let Some(stream) = self.listener.incoming().next().await {
-            let mut stream = match stream {
-                Ok(s) => s,
-                Err(err) => {
-                    log::error!("{}", err);
-                    continue;
-                }
-            };
-            let _handle = task::spawn(async move {
-                ChatServer::handle_connections(&mut stream).await;
-            });
-        }
-    }
-
-    async fn bind(port: u16) -> anyhow::Result<Self> {
-        let res = Self {
-            listener: net::TcpListener::bind(("127.0.0.1", port))
-                .await
-                .context("failed to bind adress")?,
-        };
-        log::info!("listening on {}", res.listener.local_addr()?);
-        Ok(res)
-    }
-}
+mod server;
 
 /// init the logger
 /// Log level: `info` by default, use `LOG` env to change
@@ -59,7 +12,7 @@ fn init_log() -> Result<(), log::SetLoggerError> {
 async fn main() -> anyhow::Result<()> {
     init_log()?;
 
-    let mut socket = ChatServer::bind(8000u16).await?;
+    let mut socket = server::ChatServer::bind(8000u16).await?;
 
     socket.run().await;
 
---
+++ src/server.rs
@@ -0,0 +1,48 @@
+use anyhow::{self, Context};
+use tokio::{net, prelude::*, stream::StreamExt, task};
+
+pub struct ChatServer {
+    listener: net::TcpListener,
+}
+
+impl ChatServer {
+    async fn handle_connections(stream: &mut net::TcpStream) {
+        let mut buf = bytes::BytesMut::with_capacity(64);
+        let addr = stream.peer_addr().unwrap();
+
+        log::info!("{}: Got connection", addr);
+        loop {
+            let res = stream.read_buf(&mut buf).await;
+            let res = res.or(stream.write_buf(&mut buf).await);
+            if let Ok(0) | Err(_) = res {
+                break;
+            }
+        }
+        log::info!("{}: Connection closed", addr);
+    }
+
+    pub async fn run(&mut self) {
+        while let Some(stream) = self.listener.incoming().next().await {
+            let mut stream = match stream {
+                Ok(s) => s,
+                Err(err) => {
+                    log::error!("{}", err);
+                    continue;
+                }
+            };
+            let _handle = task::spawn(async move {
+                ChatServer::handle_connections(&mut stream).await;
+            });
+        }
+    }
+
+    pub async fn bind(port: u16) -> anyhow::Result<Self> {
+        let res = Self {
+            listener: net::TcpListener::bind(("127.0.0.1", port))
+                .await
+                .context("failed to bind adress")?,
+        };
+        log::info!("listening on {}", res.listener.local_addr()?);
+        Ok(res)
+    }
+}
