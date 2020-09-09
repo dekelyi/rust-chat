f98a7aa110c7ffcc526d9c085ec19cedfa7a7a0a
Dekel Yigal <dekelyi@gmail.com>
2020-09-09 14:08:13 +0300

    refactor(ChatServer): refactored `bin/server.rs` and renamed it to `main.rs`
    Removed `::close()` and `::connected` vector, put at as variable in the `::run` method
    
    BREAKING CHANGE: ChatServer API changed, see commit

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,9 @@ use std::sync::Arc;
 use tokio::{net, prelude::*, stream::StreamExt, sync::Mutex, task};
 
 type Stream = Arc<Mutex<net::TcpStream>>;
-type Connection = (Stream, task::JoinHandle<()>);
 
 struct ChatServer {
     listener: net::TcpListener,
-    connected: Vec<Connection>,
 }
 
 impl ChatServer {
@@ -15,7 +13,7 @@ impl ChatServer {
         let mut buf = bytes::BytesMut::with_capacity(64);
         let addr = stream.lock().await.peer_addr().unwrap();
 
-        log::info!("Got connection from {}", addr);
+        log::info!("{}: Got connection", addr);
         loop {
             let res = stream.lock().await.read_buf(&mut buf).await;
             let res = res.or(stream.lock().await.write_buf(&mut buf).await);
@@ -23,10 +21,11 @@ impl ChatServer {
                 break;
             }
         }
-        log::info!("Closed connection with: {}", addr);
+        log::info!("{}: Connection closed", addr);
     }
 
     async fn run(&mut self) {
+        let mut connected: Vec<(Stream, task::JoinHandle<()>)> = Default::default();
         while let Some(stream) = self.listener.incoming().next().await {
             let stream = match stream {
                 Ok(s) => s,
@@ -42,23 +41,7 @@ impl ChatServer {
                     ChatServer::handle_connections(&mut stream).await;
                 })
             };
-            self.connected.push((stream, handle));
-        }
-    }
-
-    async fn close(&mut self) {
-        log::info!("Shuting the server down");
-        for (stream, handle) in &mut self.connected {
-            let res = async {
-                let (lock, res) = tokio::join!(stream.lock(), handle);
-                let lock: tokio::sync::MutexGuard<net::TcpStream> = lock; // FIXME rust-analyzer not inferring
-                lock.shutdown(std::net::Shutdown::Both)?;
-                res?;
-                Ok::<(), Box<dyn std::error::Error>>(())
-            };
-            if let Err(err) = res.await {
-                log::error!("{}", err)
-            }
+            connected.push((stream, handle));
         }
     }
 
@@ -67,7 +50,6 @@ impl ChatServer {
             listener: net::TcpListener::bind(("127.0.0.1", port))
                 .await
                 .context("failed to bind adress")?,
-            connected: Default::default(),
         };
         log::info!("listening on {}", res.listener.local_addr()?);
         Ok(res)
@@ -78,7 +60,6 @@ impl ChatServer {
 /// Log level: `info` by default, use `LOG` env to change
 fn init_log() -> Result<(), log::SetLoggerError> {
     let mut builder = pretty_env_logger::formatted_builder();
-    // #[allow(clippy::or_fun_call)]
     builder.parse_filters(&std::env::var("LOG").unwrap_or_else(|_| "info".to_string()));
     builder.try_init()
 }
@@ -90,7 +71,6 @@ async fn main() -> anyhow::Result<()> {
     let mut socket = ChatServer::bind(8000u16).await?;
 
     socket.run().await;
-    socket.close().await;
 
     Ok(())
 }
