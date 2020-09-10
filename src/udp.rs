a264cd2a77329c0bf0ba37fcb41a6739120ba164
Dekel Yigal <dekelyi@gmail.com>
2020-09-10 10:55:30 +0300

    refactor: Change use of `RwLock` to `Mutex`

--- src/udp.rs
+++ src/udp.rs
@@ -1,11 +1,11 @@
 use anyhow::{self, Context};
 use std::net::SocketAddr;
 use std::sync::Arc;
-use tokio::{net, sync::RwLock, task};
+use tokio::{net, sync::Mutex, task};
 
 /// a chat server instance
 pub struct UdpServer {
-    listener: Arc<RwLock<net::UdpSocket>>,
+    listener: Arc<Mutex<net::UdpSocket>>,
 }
 
 impl UdpServer {
@@ -19,30 +19,17 @@ impl UdpServer {
     pub async fn run(&mut self) {
         loop {
             let mut buf = [0u8; 32];
-            match self.listener.write().await.recv_from(&mut buf).await {
+            match self.listener.lock().await.recv_from(&mut buf).await {
                 Err(_) => continue,
                 Ok((_, addr)) => {
                     let socket = self.listener.clone();
                     let _res = task::spawn(async move {
                         let res = UdpServer::handle_msg(buf.to_vec(), addr).await;
-                        socket.write().await.send_to(&res[..], addr).await.unwrap();
+                        socket.lock().await.send_to(&res[..], addr).await.unwrap();
                     });
                 }
             }
         }
-
-        // while let Some(stream) = self.listener.incoming().next().await {
-        //     let mut stream = match stream {
-        //         Ok(s) => s,
-        //         Err(err) => {
-        //             log::error!("{}", err);
-        //             continue;
-        //         }
-        //     };
-        //     let _handle = task::spawn(async move {
-        //         UdpServer::handle_connections(&mut stream).await;
-        //     });
-        // }
     }
 
     /// Create a new instance of a bound server
@@ -50,9 +37,9 @@ impl UdpServer {
         let listener = net::UdpSocket::bind(("127.0.0.1", port))
             .await
             .context("failed to bind adress")?;
-        let listener = Arc::new(RwLock::new(listener));
+        let listener = Arc::new(Mutex::new(listener));
         let res = Self { listener };
-        log::info!("listening on {}", res.listener.read().await.local_addr()?);
+        log::info!("listening on {}", res.listener.lock().await.local_addr()?);
         Ok(res)
     }
 }
